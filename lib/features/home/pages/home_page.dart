480a859dd85be325992272b9ef6326bd1ec28243
Sebastian Śliwa <sebastiansliwa_23@wp.pl>
2023-01-30 21:00:34 +0100

    Improve displaying dates in the app

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -149,7 +149,7 @@ class _ListViewItem extends StatelessWidget {
                           ),
                           const SizedBox(height: 10),
                           Text(
-                            itemModel.relaseDate.toString(),
+                            itemModel.relaseDateFormatted(),
                           ),
                         ],
                       ),
